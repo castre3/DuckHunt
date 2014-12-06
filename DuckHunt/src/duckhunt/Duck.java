33f01b282e316a4ddd58da93d2a7fa287f2d8c5d
castre3 <emc0094@gmail.com>
2014-12-06 15:33:00 -0500

    Added some java doc as well as adding properties to make the ducks fly away after 3 clicks

--- DuckHunt/src/duckhunt/Duck.java
+++ DuckHunt/src/duckhunt/Duck.java
@@ -15,29 +15,31 @@ public class Duck implements ApplicationConstants {
 	/**
 	 * how long it takes the duck to fly across screen
 	 */
-	private static final float ACROSS_WORLD_TIME = 2.5f;  /* seconds*/
+	private static final float ACROSS_WORLD_TIME = 2.5f; /* seconds */
 	/**
 	 * The max speed the duck can go
 	 */
 	private static final float MAX_SPEED = WORLD_WIDTH / ACROSS_WORLD_TIME;
-	/*
-	 * 
+	/**
+	 * the x and y location as well as the scale
 	 */
 	private float x_, y_, scale_;
-	/*
-	 * 
+	/**
+	 * x and y velocity
 	 */
 	private float Vx_, Vy_;
-	/*
-	 * 
+	/**
+	 * Parameter that records if duck is shot
 	 */
 	private boolean shot = false;
-	/*
+	/**
+	 * this keeps of when the level is over
+	 */
+	private boolean levelEnded_ = false;
+	/**
 	 * 	
 	 */
 	private float radius_ = 0.15f;
-	
-	
 	/***
 	 * 
 	 * @param x
@@ -45,115 +47,137 @@ public class Duck implements ApplicationConstants {
 	 * @param angle
 	 * @param scale
 	 */
-	public Duck(float x, float y, float angle, 
-			float scale) {
+	public Duck(float x, float y, float angle, float scale) {
 		x_ = x;
 		y_ = y;
 		scale_ = scale;
-		
+
 		// generate speed
 		float theta = (float) (Math.PI * Math.random());
 		float v = (float) (MAX_SPEED);
 		Vx_ = v * theApp_.cos(theta);
 		Vy_ = v * theApp_.sin(theta);
 	}
-	
+
 	/*
 	 * 
 	 */
 	void draw() {
 
 		theApp_.pushMatrix();
-			
+
 		theApp_.translate(x_, y_);
-	
+
 		theApp_.strokeWeight(0.01f);
 		theApp_.stroke(0, 0, 0);
 		theApp_.ellipse(0, 0, radius_, radius_);
-			
+
 		theApp_.scale(scale_);
-		
+
 		theApp_.noStroke();
 		theApp_.fill(120, 174, 198);
-	
-		theApp_.ellipse(-50, +0, 140, 130);  //  right ear
-		theApp_.ellipse(+50, +0, 140, 130);  //  left ear
+
+		theApp_.ellipse(-50, +0, 140, 130); // right ear
+		theApp_.ellipse(+50, +0, 140, 130); // left ear
 		theApp_.ellipse(+0, +10, 150, 280);
-	
+
 		theApp_.fill(255);
 		theApp_.ellipse(-30, -40, 40, 60);
 		theApp_.fill(20, 90, 130);
 		theApp_.ellipse(-28, -45, 25, 28);
-	
+
 		theApp_.fill(255);
 		theApp_.ellipse(+30, -40, 40, 60);
 		theApp_.fill(20, 90, 130);
 		theApp_.ellipse(+32, -45, 25, 28);
-		
 
 		theApp_.popMatrix();
 	}
+
 	/**
-	 * 
+	 * moves the ducks making sure it is in bound
 	 */
-	public void animate(){
-		
-			x_ += Vx_ * 0.01f;
-			y_ += Vy_ * 0.01f;
-			
-			// hit left or right  bound bounce
-			if ((x_ < X_MIN) || (x_ >= X_MAX )) {
-				Vx_ = -Vx_;
-			}
-			
-			// hit top or bottom  bound
-			if (((y_ < Y_MIN) || (y_ >= Y_MAX )) && !shot) {
-				Vy_ = -Vy_;
-			}
-		
+	public void animate() {
+		x_ += Vx_ * 0.01f;
+		y_ += Vy_ * 0.01f;
+
+		// hit left or right bound bounce
+		if ((x_ < X_MIN) || (x_ >= X_MAX)) {
+			Vx_ = -Vx_;
+		}
+
+		// hit top or bottom bound
+		if (((y_ < Y_MIN) || (y_ >= Y_MAX)) && !shot) {
+			Vy_ = -Vy_;
+		}
 	}
+
 	/***
-	 * 
-	 * @param x
-	 * @param y
+	 * changed the location of the duck
+	 * @param x float the x location
+	 * @param y float the y location
 	 */
 	public void setPosition(float x, float y) {
 		x_ = x;
 		y_ = y;
 	}
+
 	/***
 	 * 
 	 * @return
 	 */
-	public float getX(){
+	public float getX() {
 		return x_;
 	}
+
 	/***
 	 * 
 	 * @return
 	 */
-	public float getY(){
+	public float getY() {
 		return y_;
 	}
+
 	/***
 	 * 
 	 * @param Vx
 	 */
-	public void setVx(float Vx){
+	public void setVx(float Vx) {
 		Vx_ = Vx;
 	}
+
 	/***
 	 * 
 	 * @param Vy
 	 */
-	public void setVy(float Vy){
+	public void setVy(float Vy) {
 		Vy_ = Vy;
 	}
+
 	/***
-	 * 
+	 * sets the fact that the duck has been shot
 	 */
-	public void setShot(){
+	public void setShot() {
 		shot = true;
+		levelEnded();
+	}
+	/***
+	 * sets the fact that the level has ended
+	 */
+	public void levelEnded() {
+		levelEnded_ = true;
+	}
+	/***
+	 * determines if the level has ended
+	 */
+	public boolean getLevelEnded() {
+		return levelEnded_;
+	}
+	/***
+	 * reports if the duck is shot.
+	 */
+	public boolean getShot() {
+		return shot;
 	}
 	/***
 	 * 
@@ -162,6 +186,7 @@ public class Duck implements ApplicationConstants {
 	public static void setApp(PApplet theApp) {
 		theApp_ = theApp;
 	}
+
 	/***
 	 * 
 	 * @param x
@@ -169,10 +194,10 @@ public class Duck implements ApplicationConstants {
 	 * @return
 	 */
 	public boolean isInside(float x, float y) {
-	
+
 		float dx = x - x_, dy = y - y_;
-		
-		return (dx*dx + dy*dy < radius_*radius_);
+
+		return (dx * dx + dy * dy < radius_ * radius_);
 	}
 
 }
--- DuckHunt/src/duckhunt/Main.java
+++ DuckHunt/src/duckhunt/Main.java
@@ -4,108 +4,119 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 
-
 public class Main extends PApplet implements ApplicationConstants {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private boolean initApp_ = setAppForAllClasses();
 	private int frameCounter_ = 0;
 	private int lastDrawTime_ = 0;
-	
 	private int selectedIndex = 0;
-	
+
+	/**
+	 * The list containing all of the ducks
+	 */
 	ArrayList<Duck> duck_;
-	
+	/**
+	 * Keeps track of how many times the person has hit a duck
+	 * this is for when we implement more that one duck 
+	 */
+	private int shotCount = 0;
+	/**
+	 * Keeps track of how many times the person has tried and shot
+	 */
+	private int tryCount = 0;
+
 	public void setup() {
 		size(WINDOW_WIDTH, WINDOW_HEIGHT);
-		
-		frameRate(2000);
-
+		// this is for debugging
+		// frameRate(2000);
+		frameRate(100);
 		duck_ = new ArrayList<Duck>();
-		
-		duck_.add(new Duck(WORLD_WIDTH/4, WORLD_HEIGHT/4, -PI/4, PIXELS_TO_WORLD_SCALE/4));
-		duck_.add(new Duck(WORLD_WIDTH/4, 3*WORLD_HEIGHT/4, PI/4, PIXELS_TO_WORLD_SCALE/4));
-		duck_.add(new Duck(-WORLD_WIDTH/4, WORLD_HEIGHT/4, 3*PI/4, PIXELS_TO_WORLD_SCALE/4));
-		duck_.add(new Duck(-WORLD_WIDTH/4, 3*WORLD_HEIGHT/4, 3*PI/4, PIXELS_TO_WORLD_SCALE/4));
-		
+
+		duck_.add(new Duck(WORLD_WIDTH / 4, WORLD_HEIGHT / 4, -PI / 4,PIXELS_TO_WORLD_SCALE / 4));
+		// duck_.add(new Duck(WORLD_WIDTH/4, 3*WORLD_HEIGHT/4, PI/4,PIXELS_TO_WORLD_SCALE/4));
+		// duck_.add(new Duck(-WORLD_WIDTH/4, WORLD_HEIGHT/4, 3*PI/4,PIXELS_TO_WORLD_SCALE/4));
+		// duck_.add(new Duck(-WORLD_WIDTH/4, 3*WORLD_HEIGHT/4, 3*PI/4,PIXELS_TO_WORLD_SCALE/4));
 	}
 
 	public void draw() {
+		background(127);
 		
-		
-		//Version 2 with ArrayList (new style)
 		for (Duck obj : duck_)
 			obj.animate();
 
-
-		//if (frameCounter_++ % 10 == 0)
-		//{ 
-			background(127);
-
-	 		//	We are still in pixel units.
-	 		translate(WINDOW_WIDTH/2, WINDOW_HEIGHT);
-	 		
-	 		//	change to world units
-	 		scale(WORLD_TO_PIXELS_SCALE, -WORLD_TO_PIXELS_SCALE);
-	 		
-	 		stroke(255);
-	 		strokeWeight(1*PIXELS_TO_WORLD_SCALE);
-	 		line(0, 0, 0, WORLD_HEIGHT);
-
-	 		pushMatrix();
+		// We are still in pixel units.
+		translate(WINDOW_WIDTH / 2, WINDOW_HEIGHT);
+		// change to world units
+		scale(WORLD_TO_PIXELS_SCALE, -WORLD_TO_PIXELS_SCALE);
+				
+		pushMatrix();
+		for (int i = 0; i < duck_.size(); i++) {
+			duck_.get(i).draw();
 			
-			for (Duck obj : duck_){
-				obj.draw();
-				//if (obj.getY() < -WORLD_HEIGHT){ <- We gotta remove this duck somehow but this code throws an error
-					//duck_.remove(obj);
-				//}
+			if ((duck_.get(i).getY() < -WORLD_HEIGHT  && duck_.get(i).getLevelEnded()) 
+					|| (duck_.get(i).getY() > WORLD_HEIGHT  && duck_.get(i).getLevelEnded())) { // this random amount is bc it was going out the top
+				duck_.remove(duck_.get(i));
 			}
+		}
+		popMatrix();
 
-			translate(-width, 0);
-			for (Duck obj : duck_)
-				obj.draw();
-
-			translate(2*width, 0);
-			for (Duck obj : duck_)
-				obj.draw();
-
-			popMatrix();
-		//}
 	}
-
 	public void keyReleased() {
 		switch (key) {
 		case '0':
 		case '1':
 		case '2':
 		case '3':
-		selectedIndex = Integer.parseInt(""+key);
-		break;
+			selectedIndex = Integer.parseInt("" + key);
+			break;
 		}
-		
-		
 	}
+
+	/**
+	 * This controls what happens when you shoot the duck
+	 */
 	public void mouseReleased() {
-//		face_.get(selectedIndex).setPosition(xPixelToWorld(mouseX), yPixelToWorld(mouseY));
-		
 		float x = xPixelToWorld(mouseX), y = yPixelToWorld(mouseY);
 
-		for (Duck obj : duck_)
+		for (Duck obj : duck_) {
 			if (obj.isInside(x, y)) {
-				obj.setVx(0);		//temporarily make it not move
+				obj.setVx(0); // temporarily make it not move
 				obj.setVy(0);
-				delay(1000);		//do the "pause" thing
-				obj.setVy(-1f);		//make it fall off screen
-				obj.setShot();		//Set "shot" to "false"
-				//duck_.remove(obj);
-				break;
-			}
+				delay(500); // do the "pause" thing
+				obj.setVy(-1f); // make it fall off screen
+				obj.setShot(); // Set "shot" to "false"
+				shotCount++;
+				tryCount++;
+				checkLevelComplete();
+				break; 
+			}	
+		}
+		tryCount++;
+		println("Try count for click:" + tryCount);
+		checkLevelComplete();
 	}
+	/**
+	 * this checks if the person has shot all of the ducks in play
+	 * 1 for now but added so that can expand to multiple ducks
+	 */
+	private void checkLevelComplete() {
+		//if the person has used all of their chances. 
+		if (tryCount == 3) {
+			
+			for (Duck obj : duck_) {
+				obj.levelEnded();
+				obj.setVx(0); // temporarily make it not move
+				obj.setVy(0);
+				delay(500); // do the "pause" thing
+				obj.setVy(1f); // make it fall off screen
+				break; 
+			}
+		}
 		
+	}
+
 	private float xPixelToWorld(int ix) {
 		return X_MIN + PIXELS_TO_WORLD_SCALE * ix;
 	}
@@ -115,9 +126,7 @@ public class Main extends PApplet implements ApplicationConstants {
 	}
 
 	private boolean setAppForAllClasses() {
-
 		Duck.setApp(this);
-
 		return true;
 	}
 }
