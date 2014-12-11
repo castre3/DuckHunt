3de11645a394aacb643a3f6614a2fe53d72b2fa9
castre3 <emc0094@gmail.com>
2014-12-11 18:08:18 -0500

    should be working sprite and working timer

--- DuckHunt/src/duckhunt/Main.java
+++ DuckHunt/src/duckhunt/Main.java
@@ -1,4 +1,5 @@
 package duckhunt;
+
 import java.util.ArrayList;
 
 import processing.core.PApplet;
@@ -9,17 +10,13 @@ public class Main extends PApplet implements ApplicationConstants {
 	private static final long serialVersionUID = 1L;
 
 	private boolean initApp_ = setAppForAllClasses();
-	
-	private static int frameCounter_ = 0;
-
-
 	/**
 	 * The list containing all of the ducks
 	 */
 	ArrayList<Duck> duck_;
 	/**
-	 * Keeps track of how many times the person has hit a duck
-	 * this is for when we implement more that one duck 
+	 * Keeps track of how many times the person has hit a duck this is for when
+	 * we implement more that one duck
 	 */
 	private int shotCount = 0;
 	/**
@@ -27,116 +24,131 @@ public class Main extends PApplet implements ApplicationConstants {
 	 */
 	private int tryCount = 0;
 	/**
-	 * The background.
+	 * The background texture
 	 */
 	PImage _backgroundImage;
-	
+	/**
+	 * the background instance object
+	 */
 	Background background_;
+	/**
+	 * frame counter variable for animation
+	 */
 	private int frameCounter = 0;
-	
+	/**
+	 * The sprite containing all of the characters
+	 */
+	PImage sprite_; 
+	/**
+	 * The timer instance
+	 */
 	Timer time_;
-	boolean dogAnimate_;  //The value of dogAnimate we will be checking
-
-	public static boolean dogAnimateValueFromThread = false; //A static variable that our thread
+	/**
+	 * the current value when the dog vs duck on screen location
+	 * current working on
+	 */
+	boolean dogAnimate_ = false; // The value of dogAnimate we will be checking
+	/**
+	 * the current value when the dog vs duck on screen location
+	 * the value the timer is returning
+	 */
+	private static boolean dogAnimateValueFromThread = false; // A static variable that our thread
+	/**
+	 * end early switch for timer
+	 */
+	private static boolean switchTimerValue = false; 
 
 	public void setup() {
-		size(WINDOW_WIDTH, WINDOW_HEIGHT,P3D);
-		
-		Timer time_ = new Timer(1000,2000);
+		size(WINDOW_WIDTH, WINDOW_HEIGHT, P3D);
+		//Timer(dog, duck)
+		Timer time_ = new Timer(1000, 2000);
 		Thread timerThread = new Thread(time_);
 		timerThread.start();
-		
-		frameRate(2000);	
-		
+
+		frameRate(2000);
+
 		duck_ = new ArrayList<Duck>();
-		
-		//load image 0-1
+
+		// load image 0-1
 		textureMode(NORMAL);
-		
-		//Image loading section
-		PImage sprite_=loadImage("sprite.png");
-		_backgroundImage=loadImage("background.jpg");
+
+		// Image loading section
+		sprite_ = loadImage("sprite.png");
+		_backgroundImage = loadImage("background.jpg");
 		PImage ground_ = loadImage("ground.jpg");
 		PImage allSprite_ = loadImage("duckhunt_various_sheet.png");
-		
-		background_ = new Background(ground_,_backgroundImage, allSprite_);
-		// the y parameter needs to be above WORLD_HEIGHT/3
-		duck_.add(new Duck(sprite_,WORLD_WIDTH /2, WORLD_HEIGHT / 2, -PI / 4,PIXELS_TO_WORLD_SCALE / 4));
+
+		background_ = new Background(ground_, _backgroundImage, allSprite_);
 	}
 
-	public void draw() {;
-		//Background world setup - etc
+	public void draw() {
+		// Background world setup - etc
 		background_.draw();
-
-		if(getStatic() != dogAnimate_) {
-			dogAnimate_ = getStatic();
-			if(dogAnimate_== false) {
-				//Destroy dog object
-				//Instantiate duck object
-			}
-			else {
-				//Destroy duck object
-				//Instantiate dog object
-			}
-		}
-		if(dogAnimate_ = false) {
-			//draw duck
-			//animate duck
-		}
-		else {
-			//draw dog
-			//animate dog
-		}
-		
-		if (frameCounter++ % 10 == 0) 
-		{
-			for (Duck obj : duck_)
-				obj.switchBirdWing();
-		}
-		for (Duck obj : duck_)
-			obj.animate();
-
 		// We are still in pixel units.
 		translate(1, WINDOW_HEIGHT);
-		
 		// change to world units
-		scale(WORLD_TO_PIXELS_SCALE, -WORLD_TO_PIXELS_SCALE);
-				
-		pushMatrix();
-		for (int i = 0; i < duck_.size(); i++) {
-			duck_.get(i).draw();
-			
-			if ((duck_.get(i).getY() < -WORLD_HEIGHT  && duck_.get(i).getLevelEnded()) 
-					|| (duck_.get(i).getY() > WORLD_HEIGHT  && duck_.get(i).getLevelEnded())) { // this random amount is bc it was going out the top
-				duck_.remove(duck_.get(i));
+		scale(WORLD_TO_PIXELS_SCALE, -WORLD_TO_PIXELS_SCALE);	
+
+		if (dogAnimateValueFromThread != dogAnimate_) { 
+			dogAnimate_ = getDogAnimateValueFromThread();
+			if (dogAnimate_ == false) {
+				// Destroy dog object
+				// Instantiate duck object
+				// the y parameter needs to be above WORLD_HEIGHT/3
+				duck_.add(new Duck(sprite_, WORLD_WIDTH / 2, WORLD_HEIGHT / 2, -PI / 4));
+			} else {
+				// Destroy duck object
+				duck_.clear();
+				// Instantiate dog object
 			}
 		}
-		popMatrix();
+		
+		//make sure the duck exists
+		if (!duck_.isEmpty()) {
+			//create the wing animation for the duck
+			if (frameCounter++ % 10 == 0) {
+				for (Duck obj : duck_)
+					obj.switchBirdWing();
+			} 
+			
+			//move the duck
+			for (Duck obj : duck_)
+				obj.animate();
+			
+			//make sure the bird is in range and in play, if not remove
+			for (int i = 0; i < duck_.size(); i++) {
+				duck_.get(i).draw();
+				if ((duck_.get(i).getY() < -WORLD_HEIGHT && duck_.get(i).getLevelEnded())
+						|| (duck_.get(i).getY() > WORLD_HEIGHT && duck_.get(i).getLevelEnded())) {
+					duck_.remove(duck_.get(i));
+				}
+			}
+		}
 	}
-	
-
 
 	/**
 	 * 
 	 */
 	public void keyReleased() {
-		/*switch (key) {
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-			int selectedIndex = Integer.parseInt("" + key);
-			break;
-		}*/
-		if (key=='1'){
-			camera(width/2.0f, height/2.0f, (height/2.0f) / tan(PI*30.0f / 180.0f), width/2.0f, height/2.0f, 0, 0, 1, 0); 
-			
-		} else if (key=='2'){
-			camera(width/2.0f, height/1.0f, (height/1.5f) / tan(PI*30.0f / 180.0f), width/2.0f, height/2.0f, 0, 0, 1, 0);
-		
-		} else if (key=='3'){
-			camera(width/2.0f, height*2.5f, (height/3.0f) / tan(PI*30.0f / 180.0f), width/2.0f, height/2.0f, 0, 0, 1, 0); 
-			
+		/*
+		 * switch (key) { case '0': case '1': case '2': case '3': int
+		 * selectedIndex = Integer.parseInt("" + key); break; }
+		 */
+		if (key == '1') {
+			camera(width / 2.0f, height / 2.0f, (height / 2.0f)
+					/ tan(PI * 30.0f / 180.0f), width / 2.0f, height / 2.0f, 0,
+					0, 1, 0);
+
+		} else if (key == '2') {
+			camera(width / 2.0f, height / 1.0f, (height / 1.5f)
+					/ tan(PI * 30.0f / 180.0f), width / 2.0f, height / 2.0f, 0,
+					0, 1, 0);
+
+		} else if (key == '3') {
+			camera(width / 2.0f, height * 2.5f, (height / 3.0f)
+					/ tan(PI * 30.0f / 180.0f), width / 2.0f, height / 2.0f, 0,
+					0, 1, 0);
+
 		}
 	}
 
@@ -145,7 +157,7 @@ public class Main extends PApplet implements ApplicationConstants {
 	 */
 	public void mouseReleased() {
 		float x = xPixelToWorld(mouseX), y = yPixelToWorld(mouseY);
-
+		boolean wasInside = false;
 		for (Duck obj : duck_) {
 			if (obj.isInside(x, y)) {
 				obj.setVx(0); // temporarily make it not move
@@ -154,31 +166,35 @@ public class Main extends PApplet implements ApplicationConstants {
 				obj.setVy(-1f); // make it fall off screen
 				obj.setShot(); // Set "shot" to "false"
 				shotCount++;
-				tryCount++;
-				checkLevelComplete();
-				break; 
-			}	
+				wasInside = true;
+				break;
+			}
 		}
 		tryCount++;
 		println("Try count for click:" + tryCount);
-		checkLevelComplete();
+		checkLevelComplete(wasInside);
 	}
+
 	/**
-	 * this checks if the person has shot all of the ducks in play
-	 * 1 for now but added so that can expand to multiple ducks
+	 * this checks if the person has shot all of the ducks in play 1 for now but
+	 * added so that can expand to multiple ducks
+	 * @param wasInside
 	 */
-	private void checkLevelComplete() {
-		//if the person has used all of their chances. 
+	private void checkLevelComplete(boolean wasInside) {
+		// if the person has used all of their chances.
+
 		if (tryCount == 3) {
 			for (Duck obj : duck_) {
 				obj.levelEnded();
-				obj.setVx(0); // temporarily make it not move
-				obj.setVy(0);
-				delay(500); // do the "pause" thing
-				obj.setVy(1f); // make it fly up screen
+				if (!wasInside) {
+					obj.setVx(0); // temporarily make it not move
+					obj.setVy(0);
+					delay(500); // do the "pause" thing
+					obj.setVy(1f); // make it fly up screen
+				}
 			}
 		}
-		
+
 	}
 
 	private float xPixelToWorld(int ix) {
@@ -188,20 +204,41 @@ public class Main extends PApplet implements ApplicationConstants {
 	private float yPixelToWorld(int iy) {
 		return Y_MIN + PIXELS_TO_WORLD_SCALE * (WINDOW_HEIGHT - iy);
 	}
-
+	/**
+	 * 
+	 * @param value
+	 */
+	public static void setDogAnimateValueFromThread(boolean value) {
+		dogAnimateValueFromThread = value;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static boolean getDogAnimateValueFromThread() {
+		return dogAnimateValueFromThread;
+	}
+	/**
+	 * 
+	 */
+	public static void resetSwitchTimerValue() {
+		switchTimerValue = false;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static boolean getSwitchTimerValue() {
+		return switchTimerValue;
+	}
+	/**
+	 * 
+	 * @return
+	 */
 	private boolean setAppForAllClasses() {
 		Duck.setApp(this);
 		Background.setApp(this);
 		Timer.setApp(this);
 		return true;
 	}
-	
-	public static void setStatic(boolean value){
-		dogAnimateValueFromThread = value;
-	}
-	
-	public static boolean getStatic(){
-		return dogAnimateValueFromThread;
-	}
-
 }
